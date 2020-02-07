c99a32dbf5b75963d24ba79dcfb1fe57b030a887
Jem Cairns <jem.cairns@ucdconnect.ie>
2020-02-07 13:51:07 +0000

    first practical working

---
+++ my_project/src/main/java/uni/system/my_project/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package uni.system.my_project.repository;
+
+import uni.system.my_project.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {}
