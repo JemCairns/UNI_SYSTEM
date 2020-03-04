7d66615e75cd0f8f7f8bad6616dddd54438945e0
AfroAl <woodysandrew1A>
2020-03-04 09:37:10 +0000

    Cleaner fix for error messages

--- my_project/src/main/java/uni/system/webapp/modules/ModulesController.java
+++ my_project/src/main/java/uni/system/webapp/modules/ModulesController.java
@@ -66,11 +66,9 @@ public class ModulesController {
 
         if(userID.endsWith("STU")) {
 
-            boolean feesDue=false;
             if (service.getStudent(userID).getFees_due() > 0) {
                 model.addAttribute("maxStudents", false);
                 model.addAttribute("feesDue", true);
-                feesDue=true;
             } else {
 
                 if(formType.equals("dropout")) {
@@ -106,10 +104,7 @@ public class ModulesController {
             model.addAttribute("modNotRegs", moduleNotRegs);
             model.addAttribute("top", topics);
             model.addAttribute("top_reg", topicRegs);
-            if(feesDue){
-                return "modules";
-            }
-            return "redirect:modules";
+            return "modules";
         }
         else{
             String studentID = service.getStudent(userID).getID();
