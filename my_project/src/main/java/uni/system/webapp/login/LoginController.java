eb50d8bea0a26ca94278c9c714c2622624370253
Jem Cairns <jem.cairns@ucdconnect.ie>
2020-05-14 14:21:01 +0100

    captcha added to login page

--- my_project/src/main/java/uni/system/webapp/login/LoginController.java
+++ my_project/src/main/java/uni/system/webapp/login/LoginController.java
@@ -1,9 +1,12 @@
 package uni.system.webapp.login;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import uni.system.webapp.captcha.ReCaptchaResponse;
 import uni.system.webapp.logger.Logging;
 import uni.system.webapp.tables.Student;
 
@@ -16,16 +19,34 @@ public class LoginController {
     @Autowired
     LoginService service;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String showLoginPage() {
         return "login";
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String showWelcomePage(ModelMap model, @RequestParam String ID, @RequestParam String password, @RequestParam String user,  HttpSession session) {
+    public String showWelcomePage(ModelMap model,
+                                  @RequestParam String ID,
+                                  @RequestParam String password,
+                                  @RequestParam String user,
+                                  @RequestParam(name="g-recaptcha-response") String captchaResponse,
+                                  HttpSession session
+    ) {
 
-        String userType = service.validateUser(ID, password, user);
-        boolean invalidUser = userType.equals("");
+        String url = "https://www.google.com/recaptcha/api/siteverify";
+        String params = "?secret=6LctyPYUAAAAAM45kposcNT1V-vYiUYhpt98aTsu&response="+captchaResponse;
+
+        ReCaptchaResponse reCaptchaResponse = restTemplate.exchange(url+params, HttpMethod.POST, null, ReCaptchaResponse.class).getBody();
+
+        String userType="";
+        boolean invalidUser = true;
+        if(reCaptchaResponse.isSuccess()) {
+            userType = service.validateUser(ID, password, user);
+            invalidUser = userType.equals("");
+        }
 
         if(invalidUser) {
             // login failure
